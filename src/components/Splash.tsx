f847a6b3e75616afc370fcf6480586e5063968a6
cuingskot76 <afrizapamujo@gmail.com>
2023-05-29 10:12:19 +0700

    feat: add splash screen

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,10 +1,26 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import {View, Image} from 'react-native';
+import React, {useEffect} from 'react';
 
-const Splash = () => {
+const Splash = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Main');
+    }, 3000);
+  }, [navigation]);
+
   return (
     <View>
-      <Text>Splash</Text>
+      <Image
+        source={require('../../public/images/music-man.png')}
+        style={{
+          width: 300,
+          height: 300,
+          marginTop: '50%',
+          justifyContent: 'center',
+          alignSelf: 'center',
+        }}
+      />
     </View>
   );
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,22 @@
 import React from 'react';
+import {View} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Splash from '../components/Splash';
 import Home from '../screens/Home/Home';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Profile from '../screens/Profile/Profile';
-import Discover from '../screens/Discover/Discover';
-import ButtonTab from '../components/ButtonTab';
-import Favorite from '../screens/Favorite/Favorite';
 import DetailPlayer from '../components/DetailPlayer';
 import ArtistDetail from '../components/ArtistDetail';
 import Notification from '../components/Notification';
+import Splash from '../components/Splash';
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-// const BottomTabBar = item => {
-//   return <ButtonTab {...item} />;
-// };
 
 export const HomeStack = () => {
   return (
-    <Stack.Navigator initialRouteName="Main">
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen
+        name="Splash"
+        component={Splash}
+        options={{headerShown: false}}
+      />
       <Stack.Screen
         name="Main"
         component={Home}
@@ -49,66 +45,8 @@ export const HomeStack = () => {
   );
 };
 
-// const BottomNavigator = () => {
-//   return (
-//     <Tab.Navigator tabBar={props => BottomTabBar(props)}>
-//       <Tab.Screen name="Home" component={Home} options={{headerShown: false}} />
-//       <Tab.Screen
-//         name="Discover"
-//         component={Discover}
-//         options={{headerShown: false}}
-//       />
-//       <Tab.Screen
-//         name="Favorite"
-//         component={Favorite}
-//         options={{headerShown: false}}
-//       />
-//       <Tab.Screen
-//         name="Profile"
-//         component={Profile}
-//         options={{headerShown: false}}
-//       />
-//     </Tab.Navigator>
-//   );
-// };
-
 const Router = () => {
-  return (
-    // <Stack.Navigator initialRouteName="Main">
-    //   <Stack.Screen
-    //     name="Splash"
-    //     options={{headerShown: false}}
-    //     component={Splash}
-    //   />
-    //   <Stack.Screen
-    //     name="Main"
-    //     options={{headerShown: false}}
-    //     component={BottomNavigator}
-    //   />
-    //   <Stack.Screen
-    //     name="DetailPlayer"
-    //     options={{
-    //       headerShown: false,
-    //     }}
-    //     component={DetailPlayer}
-    //   />
-    //   <Stack.Screen
-    //     name="DetailArtist"
-    //     options={{
-    //       headerShown: false,
-    //     }}
-    //     component={ArtistDetail}
-    //   />
-    //   <Stack.Screen
-    //     name="Notification"
-    //     options={{
-    //       headerShown: false,
-    //     }}
-    //     component={Notification}
-    //   />
-    // </Stack.Navigator>
-    <View></View>
-  );
+  return <View />;
 };
 
 export default Router;

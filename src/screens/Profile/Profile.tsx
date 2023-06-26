2928dc6d89515746d37b20fd9c98ed5fdcb5830a
cuingskot76 <afrizapamujo@gmail.com>
2023-06-26 11:17:59 +0700

    feat: add json-server

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,22 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState} from 'react';
-import {
-  Alert,
-  BackHandler,
-  ScrollView,
-  StatusBar,
-  Switch,
-  Text,
-  View,
-} from 'react-native';
+import {ScrollView, Image, View, Text} from 'react-native';
 import Button from '../../components/atom/Button';
 import {COLORS} from '../../constants/theme';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useNavigation} from '@react-navigation/native';
 import {UseAccessTokenStore} from '../../components/connect/Login';
 
 const Profile = () => {
-  // const res = UseAccessTokenStore(state => state.accessToken);
+  const [data, setData] = useState([]);
 
   const handleLogOut = () => {
     AsyncStorage.removeItem('accessToken');
@@ -25,6 +16,26 @@ const Profile = () => {
     AsyncStorage.removeItem('tokenExp');
     UseAccessTokenStore.setState({tokenExp: null});
   };
+
+  const fetchDataProfile = async () => {
+    try {
+      const res = await fetch('http://10.0.2.2:3000/profile', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const dataJson = await res.json();
+      setData(dataJson);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDataProfile();
+  }, []);
+
   return (
     <ScrollView
       contentContainerStyle={{
@@ -33,6 +44,29 @@ const Profile = () => {
         flex: 1,
         backgroundColor: COLORS.dark,
       }}>
+      <Image
+        source={require('../../../public/images/profile-image/me.png')}
+        style={{width: 200, height: 200, borderRadius: 100, marginTop: 20}}
+      />
+
+      {data
+        ? data?.map(item => (
+            <View style={{alignItems: 'center'}}>
+              <Text
+                style={{
+                  color: 'white',
+                  fontSize: 20,
+                  marginTop: 20,
+                  marginBottom: 5,
+                  fontWeight: 'bold',
+                }}>
+                {item?.name}
+              </Text>
+              <Text style={{color: 'white', fontSize: 16}}>{item?.nim}</Text>
+            </View>
+          ))
+        : null}
+
       <Button
         title="Log out"
         colorText={COLORS.white}
